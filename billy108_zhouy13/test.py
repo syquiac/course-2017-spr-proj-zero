e328bafb51b7ca5807c5f00167164e035d1da3cd
BillyYizhiHuang <billywongt-mac@hotmail.com>
2017-01-24 15:14:45 -0500

    Delete test.py

--- billy108_zhouy13/test.py
+++
@@ -1,6 +0,0 @@
-import json
-import sodapy
-
-client = sodapy.Socrata("data.cityofboston.gov", None)
-response = client.get("awu8-dc52", limit=10)
-print(json.dumps(response, sort_keys=True, indent=2))
